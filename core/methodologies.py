d6b9991946fd089b19b57f8de498bff601acb6e4
Francisco Ibanez <francisco.ibanez@gmail.com>
2023-11-28 23:31:20 -0500

    correcting small bug in methodologies that was making the residuals to potentially have empty rows

--- core/methodologies.py
+++ core/methodologies.py
@@ -1,42 +1,9 @@
-from hmmlearn.hmm import GaussianHMM
 import pandas as pd
 from scipy.linalg import block_diag
 import numpy as np
 
 
-class HMM:
-    def __init__(self, n_states, diag=False):
-        self.mdl = GaussianHMM(
-            n_components=n_states,
-            covariance_type={True: 'diag', False: 'full'}[diag],
-            n_iter=100,
-            min_covar=1e-3,
-            algorithm='viterbi',
-            random_state=0,
-            tol=.75
-        )
-        self.emission_prob = None
-        self.mu = None
-        self.sigma = None
-        self.transition_matrix = None
-        self.bic = None
-        self.aic = None
-
-    def fit(self, training_sample):
-        self.mdl.fit(training_sample)
-        gamma = self.mdl.predict_proba(training_sample)
-        self.emission_prob = pd.DataFrame(gamma, index=training_sample.index)
-        tmat = pd.DataFrame(self.mdl.transmat_)
-        tmat.index.name = 'From'
-        tmat.columns.name = 'To'
-        self.transition_matrix = tmat
-        self.mu = self.mdl.means_
-        self.sigma = self.mdl.covars_
-        self.bic = self.mdl.bic(training_sample)
-        self.aic = self.mdl.aic(training_sample)
-
-
-class RegimeConditionalLS:
+class RegimeWeightedLS:
     # sigma * I (check large scale portfolio opt paper)
     def __init__(self, exog, endog, emission_prob):
         self.endog = endog.copy()
@@ -74,7 +41,7 @@ class RegimeConditionalLS:
         res = pd.DataFrame(betas, index=self._param_labels, columns=self._state_labels)
         self.params = res.copy()
 
-        self.resid = pd.DataFrame(index=self.endog.index, columns=self._state_labels)
+        self.resid = pd.DataFrame(index=cmn_idx, columns=self._state_labels)
         self.sigma = pd.Series(index=self._state_labels)
         for s in range(self.m):
             b = betas[:, s].reshape(-1, 1)
@@ -86,7 +53,7 @@ class RegimeConditionalLS:
     def predict(self, transition_matrix, as_of=None, horizon=1):
         betas = self.params.values
         B = transition_matrix.values
-        B_h = np.linalg.matrix_power(B, horizon)
+        B_h = np.linalg.matrix_power(B, horizon)  # TODO: triple-check the orientation of these matrices
         g = self.prob.asof(as_of).values.reshape(-1, 1)
 
         self.cond_prob = pd.Series((B_h.T @ g).flatten(), index=self._state_labels)
@@ -94,7 +61,7 @@ class RegimeConditionalLS:
         self.cond_sigma = (self.sigma.values @ B_h.T @ g).item()
 
 
-class ProjectMoments:
+class ProjectMoments:  # Should be ConditionalJointDistribution
     def __init__(self, factor_mean, factor_cov, probabilities, residuals):
         pass
 
@@ -103,17 +70,4 @@ if __name__ == "__main__":
     import os
     from datetime import datetime
 
-    fldr = os.path.abspath(os.path.join(os.getcwd(), '..'))
-    X = pd.read_pickle(f'{fldr}/data/test_x.pkl')
-    Y = pd.read_pickle(f'{fldr}/data/test_y.pkl')
-    dt = datetime(2019, 3, 29)
 
-    # HMM wrapper
-    mdl = HMM(n_states=4)
-    mdl.fit(X.dropna())
-
-    # Regime conditional least squares
-    rb = RegimeConditionalLS(exog=X, endog=Y['y2'], emission_prob=mdl.emission_prob)
-    rb.fit(add_constant=True)
-    rb.predict(transition_matrix=mdl.transition_matrix, as_of=dt)
-    print(rb.params)
