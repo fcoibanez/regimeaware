a2e6a55b8bbaaa2a887b15727f82dee170d46e8f
Francisco Ibanez <francisco.ibanez@gmail.com>
2023-12-09 14:30:09 -0500

    factor projection to security space added

---
+++ core/utils.py
@@ -0,0 +1,87 @@
+"""Auxiliary function."""
+import pandas as pd
+import numpy as np
+from regimeaware.routines.cfg import factor_set
+
+
+def looking_fwd_prob(transmat: pd.DataFrame, current_emission_prob: pd.Series, horizon: int = 1):
+    """Looking-forward emission probabilities.
+
+    Combines the transition matrix and current emission probabilities to come up with
+    ex-ante probabilities at a specific horizon (number of periods) used to get expected
+    values.
+
+    :param transmat:
+    :param current_emission_prob:
+    :param horizon:
+    :return:
+    """
+    B = transmat.values
+    B_h = np.linalg.matrix_power(B, horizon)
+    g = current_emission_prob.values.reshape(-1, 1)
+    g_t = B_h.T @ g
+
+    return pd.Series(g_t.flatten(), index=transmat.index)
+
+
+def unpack_betas(raw):
+    """xyz.
+
+    :param raw:
+    :return:
+    """
+    res = pd.pivot_table(raw.reset_index(name='value'), index='id', columns='factor', values='value')
+    return res[['const'] + factor_set]
+
+
+def project_covariances(betas: pd.DataFrame, factor_covariance, residual_variances):
+    """
+
+    :param betas: n-by-k
+    :param factor_covariance:
+    :param residual_variances:
+    """
+    theta = betas.reindex(factor_covariance.index, axis=1).values.T
+    sigma_f = factor_covariance.values
+    sigma_e = np.diag(residual_variances.reindex(betas.index))
+    sigma_s = theta.T @ sigma_f @ theta + sigma_e
+    res = pd.DataFrame(sigma_s, index=betas.index, columns=betas.index)
+    return res
+
+
+def project_means(betas, factor_means):
+    """
+
+    :param betas:
+    :param factor_means:
+    """
+    theta = betas.values.T
+    mu_f = factor_means.reindex(betas.columns)
+    if 'const' in mu_f.index:
+        mu_f['const'] = 1
+    mu = theta.T @ mu_f.values.reshape(-1, 1)
+    res = pd.Series(mu.flatten(), index=betas.index)
+    return res
+
+
+def expected_covar(conditional_covars, probs):
+    """Combines probabilities and regime covariance matrices.
+
+    :param conditional_covars:
+    :param probs:
+    :return:
+    """
+    w = probs.copy()
+    w.index.name = "state"
+    res = conditional_covars.mul(w, axis=0).groupby('factor').sum().reindex(conditional_covars.columns)
+    return res
+
+
+def expected_means(conditional_means, probs):
+    """Combines probabilities and regime means vector.
+
+    :param conditional_means:
+    :param probs:
+    """
+    res = conditional_means.mul(probs, axis=0).sum(axis=0)
+    return res
---
+++ routines/project_moments.py
@@ -0,0 +1,51 @@
+"""Projects the factor joint distribution moments onto security space."""
+import core.utils
+
+if __name__ == "__main__":
+    import pandas as pd
+    from regimeaware.core import utils
+    from regimeaware.routines import cfg
+    from tqdm import tqdm
+
+    betas = pd.read_pickle(f'{cfg.data_fldr}/exposures/forecasted_betas.pkl')
+    var = pd.read_pickle(f'{cfg.data_fldr}/exposures/var.pkl')
+    mu = pd.read_pickle(f'{cfg.data_fldr}/regimes/mu.pkl')
+    sigma = pd.read_pickle(f'{cfg.data_fldr}/regimes/sigma.pkl')
+    transmat = pd.read_pickle(f'{cfg.data_fldr}/regimes/transmat.pkl')
+    emission_prob = pd.read_pickle(f'{cfg.data_fldr}/regimes/emission_prob.pkl')
+
+    rebalance_dates = pd.date_range(cfg.bt_start_dt, cfg.bt_end_dt, freq=cfg.rebalance_freq)
+    collect_mu = []
+    collect_sigma = []
+
+    for dt in tqdm(rebalance_dates):
+        g_t = utils.looking_fwd_prob(
+            transmat=transmat.xs(dt),
+            current_emission_prob=emission_prob.xs(dt).xs(dt),
+            horizon=cfg.forecast_horizon
+        )
+
+        theta = utils.unpack_betas(betas.xs(dt))
+        sec_ids = list(theta.index)
+        mu_factor = utils.expected_means(conditional_means=mu.xs(dt), probs=g_t)
+        sigma_factor = utils.expected_covar(conditional_covars=sigma.xs(dt), probs=g_t)
+
+        mu_t = utils.project_means(betas=theta, factor_means=mu_factor)
+        sigma_t = utils.project_covariances(betas=theta, factor_covariance=sigma_factor, residual_variances=var.xs(dt))
+
+        # Collect the results
+        mu_t = mu_t.reindex(sec_ids)
+        sigma_t = sigma_t.loc[sec_ids, sec_ids]
+
+        idx = pd.MultiIndex.from_tuples([(dt, x) for x in sec_ids])
+        mu_t.index = idx
+        sigma_t.index = idx
+        collect_mu += [mu_t]
+        collect_sigma += [sigma_t]
+
+    # Save finalized results
+    res_sigma = pd.concat(collect_sigma, axis=0)
+    res_mu = pd.concat(collect_mu, axis=0)
+    res_sigma.to_pickle(f'{cfg.data_fldr}/moments/sigma.pkl')
+    res_mu.to_pickle(f'{cfg.data_fldr}/moments/mu.pkl')
+
