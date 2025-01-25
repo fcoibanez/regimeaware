45a4c28b61358901268483d957090695c0dac151
fcoibanez <francisco.ibanez@gmail.com>
2025-01-25 17:33:41 -0500

    removing micro cap

--- routines/cfg.py
+++ routines/cfg.py
@@ -12,12 +12,13 @@ bt_end_dt = datetime(2022, 12, 31)  # Backtest end
 
 factor_set = ["mktrf", "smb", "hml", "rmw", "cma"]  # Factors to study
 
-n_states = 4  # Number of hidden states to use
-estimation_freq = "W-Wed"
+n_states = 3  # Number of hidden states to use
+estimation_freq = "M"
 rebalance_freq = "M"
-data_freq = "W-Wed"
-obs_thresh = 260  # Five years of observations before estimating the RWLS
-forecast_horizon = 4  # In months
+data_freq = "M"
+obs_thresh = 180  # 15 years of observations before estimating the RWLS
+forecast_horizon = 1  # In months
+mcap_thresh = .975  # Getting rid of micro-caps
 
 ols_windows = [36, 60, 120, 999]  # Lookback windows used in the OLS estimation (months)
 gamma_iter = range(10, 110, 10)
@@ -29,6 +30,6 @@ hm_min_covar = 1E-3
 hm_iter = 100
 hm_algo = "viterbi"
 hm_rs = 1234
-hm_tol = 1E-2
+hm_tol = 1E-10
 hm_params_to_estimate = "tmc"
 hm_init_params_to_estimate = ""
--- routines/tradable_flags.py
+++ routines/tradable_flags.py
@@ -2,16 +2,31 @@ if __name__ == "__main__":
     import pandas as pd
     from regimeaware.routines import cfg
 
+    rebalance_dates = pd.date_range(cfg.bt_start_dt, cfg.bt_end_dt, freq=cfg.rebalance_freq)
+
+    # Stock must have industry mapping and price information on rebalance date
     gics_mapping = pd.read_pickle(f'{cfg.data_fldr}/gics_mapping.pkl')['sector']
-    crsp = pd.read_pickle(f'{cfg.data_fldr}/crsp_daily.pkl')[['ret', 'shrout', 'prc']]
+    crsp = pd.read_pickle(f'{cfg.data_fldr}/crsp.pkl')[['ret', 'shrout', 'prc']]
     crsp = crsp.join(gics_mapping, how='left', on='permno')
+    crsp_dates = crsp.index.get_level_values('date').unique()
+    crsp_flags = crsp.all(axis=1)
+    crsp_flags = crsp_flags.rename({x: crsp_dates.asof(x) for x in rebalance_dates})
+    mcap = crsp['shrout'].mul(crsp['prc'].abs())
+    total_mcap = mcap.groupby('date').sum()
+    mcap_sorted = mcap.groupby('date', group_keys=False).apply(pd.Series.sort_values, **{'ascending': False})
+    mcap_coverage = mcap_sorted.groupby('date').cumsum().div(total_mcap)
+    mcap_flags = mcap_coverage < cfg.mcap_thresh
+    mcap_flags = mcap_flags.reindex(mcap.index).fillna(False)
+    mcap_flags = mcap_flags.rename({x: crsp_dates.asof(x) for x in rebalance_dates})
+
+    # Stock must have factor loadings on rebalance date
     factor_loadings = pd.read_pickle(f'{cfg.data_fldr}/exposures/forecasted_betas.pkl')
+    loadings_dates = crsp.index.get_level_values('date').unique()
     loadings_flags = factor_loadings.groupby(['date', 'id']).all().rename('loadings')
     loadings_flags.index.rename({'id': 'permno'}, inplace=True)
-    rebalance_dates = factor_loadings.index.get_level_values('date').unique()
-    crsp_dates = crsp.index.get_level_values('date').unique()
-    loadings_flags = loadings_flags.rename({x: crsp_dates.asof(x) for x in rebalance_dates})
-    df = crsp.join(loadings_flags)
-    is_tradable = df.dropna().all(axis=1)
-    is_tradable = is_tradable.rename({crsp_dates.asof(x): x for x in rebalance_dates})
+    loadings_flags = loadings_flags.rename({x: loadings_dates.asof(x) for x in rebalance_dates})
+
+    # Tradable flags
+    df = crsp_flags.to_frame('crsp').join(mcap_flags.to_frame('mcap')).join(loadings_flags.to_frame('loadings'))
+    is_tradable = df.loc[rebalance_dates].fillna(False).all(axis=1)
     is_tradable.to_pickle(f'{cfg.data_fldr}/is_tradable.pkl')
