0ffcb9e17e1cb77c9909b79fa0389762e3a0262e
fcoibanez <francisco.ibanez@gmail.com>
2025-01-26 14:59:26 -0500

    updating config file

--- routines/cfg.py
+++ routines/cfg.py
@@ -8,7 +8,7 @@ data_fldr = f"{fldr}/data"
 
 trn_start_dt = datetime(1963, 7, 31)  # Training start
 bt_start_dt = datetime(2002, 12, 31)  # Backtest start
-bt_end_dt = datetime(2022, 12, 31)  # Backtest end
+bt_end_dt = datetime(2023, 12, 31)  # Backtest end
 
 factor_set = ["mktrf", "smb", "hml", "rmw", "cma"]  # Factors to study
 
@@ -25,11 +25,11 @@ gamma_iter = range(10, 110, 10)
 tev_budget_iter = np.arange(.01, .105, .005)
 
 # Hidden Markov Model parameters
-hm_cov = "full"
+hm_cov = "diag"
 hm_min_covar = 1E-3
 hm_iter = 100
 hm_algo = "viterbi"
-hm_rs = 1234
+hm_rs = 123
 hm_tol = 1E-10
 hm_params_to_estimate = "tmc"
 hm_init_params_to_estimate = ""
